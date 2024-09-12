a3bb64f9ae7484d0499507c7f40456ec08d6548e
Jonibek Uralov <anonymousbycreated@gmail.com>
2024-09-12 12:35:40 +0500

    add files

---
+++ 1-lesson.py
@@ -0,0 +1,85 @@
+# kun = input("Bugun nima kun?")
+# harorat = float(input("Havo harorati qanday?"))
+# if kun.lower()=='yakshanba' and harorat>=30:
+#     print("Cho'milgani ketdik!")
+# elif kun.lower()=='yakshanba' and harorat<30:
+#     print("Uyda dam olamiz!")
+#
+
+# narh = 15000 # mijoz 15000 so'mga taom oldi.
+# choy = True # mijoz choy ham oldi
+# salat = True # mijoz salat olmadi
+#
+# if choy and salat: # agar mijoz choy ham salat ham olgan bo'lsa
+#     narh = narh + 10000 # narhga 10000 so'm qo'shamiz
+# elif choy or salat: # agar choy yoki salat olgan bo'lsa
+#     narh = narh + 5000 # narhga 5000 so'm qo'shamiz
+#
+# print(f"Jami {narh} so'm") # yakuniy narhni chiqaramiz
+
+
+# narh = 15000  # mijoz 15 so'mga ovqat oldi
+# choy = True
+# salat = False
+# non = True
+# kompot = True
+# assorti = False
+# # Quyidagi har bir shart alohida tekshiriladi va bir-biriga bog'liq emas
+# if choy:  # agar choy olsa
+#     print("Mijoz choy oldi.")
+#     narh = narh + 3000
+# if salat:  # agar salat olsa
+#     print("Mijoz salat oldi.")
+#     narh = narh + 5000
+# if non:  # agar non olsa
+#     print("Mijoz non oldi.")
+#     narh = narh + 2000
+# if kompot:  # agar kompot olsa
+#     print("Mijoz kompot oldi.")
+#     narh = narh + 5000
+# if assorti:  # agar assorti olsa
+#     print("Mijoz assorti oldi.")
+#     narh = narh + 15000
+#
+# print(f"Jami {narh} so'm")
+#
+#
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# ovqat = input('Nima ovqat yeysiz?>>>')
+# if ovqat.lower() in menu:
+#     print('Buyurtma qabul qilindi.')
+# else:
+#     print('Afsuski bizda bunday ovqat yo\'q')
+
+
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# ovqat = input('Nima ovqat yeysiz?>>>')
+# if ovqat.lower() not in menu:
+#     print('Afsuski bizda bunday ovqat yo\'q')
+# else:
+#     print('Buyurtma qabul qilindi.')
+
+
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# buyurtmalar = ["osh","somsa","manti", "shashlik"]
+#
+# for taom in buyurtmalar:
+#     if taom in menu:
+#         print(f"Menuda {taom} bor")
+#     else:
+#         print(f"Kechirasiz, menuda {taom} yo'q")
+
+
+# menu = ['osh','qazonkabob','shashlik','norin','somsa']
+# buyurtmalar = ["osh","somsa","manti", "shashlik"]
+#
+# if buyurtmalar: # ro'yxatda biror element bo'lsa bu ifoda TRUE qaytaradi
+#     for taom in buyurtmalar:
+#         if taom in menu:
+#             print(f"Menuda {taom} bor")
+#         else:
+#             print(f"Kechirasiz, menuda {taom} yo'q")
+# else: # agar ro'yxat bo'sh bo'lsa
+#     print("Savatchangiz bo'sh!")
+
+
